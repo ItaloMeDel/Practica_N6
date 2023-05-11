8c59ec09dd9156f8e431dee6c69d76208fcfde5a
ItaloMeDel <108552107+ItaloMeDel@users.noreply.github.com>
2023-05-11 18:37:24 -0500

    Update SobreCargaOperador_Asterico.cpp

--- SobreCargaOperador_Asterico.cpp
+++ SobreCargaOperador_Asterico.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+class Alumno{
+    private:
+        double nota;
+
+    public:
+        Alumno(double nota){
+            this -> nota = nota;
+        }
+        double GetNota(){
+            return nota;
+        }
+};
+
+double operator * (Alumno A, Alumno B){
+    double promedio = (A.GetNota() + B.GetNota()) / 2;
+    return promedio;
+}
+
+int main(){
+    Alumno Al1(20);
+    Alumno Al2(14);
+    double Mayor = Al1 * Al2;
+    cout << Mayor << endl;
+}
